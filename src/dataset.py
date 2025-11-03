34ab91cc7089c608950e33ae3d66f25970e27cb0
abiolaks <kingsleylawani@gmail.com>
2025-11-03 12:48:59 +0100

    feat: add a line to save to csv

--- src/dataset.py
+++ src/dataset.py
@@ -5,11 +5,9 @@ import random
 import uuid
 from datetime import datetime, timedelta
 
+
 def generate_synthetic_transactions(
-    num_customers=100,
-    start_date="2024-01-01",
-    end_date="2024-03-31",
-    seed=42
+    num_customers=100, start_date="2024-01-01", end_date="2024-03-31", seed=42
 ):
     random.seed(seed)
     np.random.seed(seed)
@@ -20,21 +18,33 @@ def generate_synthetic_transactions(
 
     customers = [f"CUST_{i:04d}" for i in range(1, num_customers + 1)]
     transaction_types = ["POS", "Online", "Transfer", "ATM"]
-    merchant_categories = ["Groceries", "Electronics", "Utilities", "Fashion", "Restaurants"]
+    merchant_categories = [
+        "Groceries",
+        "Electronics",
+        "Utilities",
+        "Fashion",
+        "Restaurants",
+    ]
     locations = ["Lagos", "Abuja", "Port Harcourt", "Ibadan", "Kano"]
 
     all_transactions = []
 
     for cust in customers:
         num_txns = np.random.randint(20, 80)  # smaller volume
-        txn_times = sorted([
-            start_date + timedelta(minutes=np.random.randint(0, num_days * 24 * 60))
-            for _ in range(num_txns)
-        ])
+        txn_times = sorted(
+            [
+                start_date + timedelta(minutes=np.random.randint(0, num_days * 24 * 60))
+                for _ in range(num_txns)
+            ]
+        )
 
         for txn_time in txn_times:
             txn_type = random.choice(transaction_types)
-            merchant_cat = random.choice(merchant_categories) if txn_type in ["POS", "Online"] else np.nan
+            merchant_cat = (
+                random.choice(merchant_categories)
+                if txn_type in ["POS", "Online"]
+                else np.nan
+            )
             amount = np.random.uniform(10, 3000)
             declined = np.random.choice([0, 1], p=[0.95, 0.05])
 
@@ -58,16 +68,17 @@ def generate_synthetic_transactions(
                 "merchant_category": merchant_cat,
                 "transaction_location": random.choice(locations),
                 "is_declined": declined,
-                "is_fraud": is_fraud
+                "is_fraud": is_fraud,
             }
             all_transactions.append(ord)
 
-    df = pd.DataFrame(all_transactions).sort_values(by="transaction_time").reset_index(drop=True)
-    return df
+    df = (
+        pd.DataFrame(all_transactions)
+        .sort_values(by="transaction_time")
+        .reset_index(drop=True)
+    )
+    df.to_csv("../data/raw/base_bank_transactions.csv", index=False)
 
 
 # --- Generate dataset ---
 df = generate_synthetic_transactions()
-
-
-
